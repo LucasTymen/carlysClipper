3734f47d202fb03905fd54f9a3144e4b691c48d5
Lucas Tymen <lucas.tymen@gmail.com>
2023-04-12 00:35:59 +0200

    step_11

--- script.py
+++ script.py
@@ -6,12 +6,33 @@ last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
 
+# step 1 & 2
 for price in prices :
   total_price += price
 
+# step 3
 average_price = total_price / len(prices)
 
+# step 4
 print("average_price : ${0} ".format(average_price))
 
+# step 5
 new_prices = [price - 5 for price in prices]
+
+# step 6
 print(new_prices)
+
+# step 7
+total_revenue = 0
+
+# step 8
+for i in range(len(hairstyles)):
+  # step 9
+  total_revenue += prices[i] * last_week[i]
+
+# step 10
+print("Total Revenue : ${} ".format(total_revenue))
+
+# step 11
+# step 12
+# step 13
